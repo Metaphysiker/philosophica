061ce91a6043b504350174f9bffdb2874d4ed740
Metaphysiker <s.raess@me.com>
2016-01-30 10:04:15 +0100

    added eventsmonth

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -2,4 +2,9 @@ module CalendarHelper
   def calendar(date = Date.today, &block)
     Calendar.new(self, date, block).table
   end
+
+  def actualmonth(date)
+
+    @eventsmonth = Event.where('extract(month from date) = ?', date.month)
+  end
 end
