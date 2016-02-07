88cde6b9f67fa59cd2aafefc9bf02855730c780d
Metaphysiker <s.raess@me.com>
2016-02-07 01:12:55 +0100

    added events

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,9 +14,10 @@ class StaticPagesController < ApplicationController
   end
 
   def forschung
+    @events = Event.all
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @first_calendar_day = @date.beginning_of_month.beginning_of_week(:sunday)
-    @last_calendar_day = @date.end_of_month.end_of_week(:sunday)
+    @first_calendar_day = @date.beginning_of_month.beginning_of_week(:monday)
+    @last_calendar_day = @date.end_of_month.end_of_week(:monday)
 
     @weeks = (@first_calendar_day..@last_calendar_day).to_a.in_groups_of(7)
   end
