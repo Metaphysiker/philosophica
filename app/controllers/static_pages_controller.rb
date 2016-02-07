19741a201ad029cd4e6ce2a0c895e9e56976a75e
Metaphysiker <s.raess@me.com>
2016-02-07 01:03:47 +0100

    added functionality

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,8 +14,9 @@ class StaticPagesController < ApplicationController
   end
 
   def forschung
-    @first_calendar_day = Date.today.beginning_of_month.beginning_of_week(:sunday)
-    @last_calendar_day = Date.today.end_of_month.end_of_week(:sunday)
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @first_calendar_day = @date.beginning_of_month.beginning_of_week(:sunday)
+    @last_calendar_day = @date.end_of_month.end_of_week(:sunday)
 
     @weeks = (@first_calendar_day..@last_calendar_day).to_a.in_groups_of(7)
   end
