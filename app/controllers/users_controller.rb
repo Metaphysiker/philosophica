c821a08b6fc2659b48dd48846e796b7ee2b57f31
Metaphysiker <s.raess@me.com>
2016-02-13 23:15:22 +0100

    updated user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
     @tags = @user.tag_list
+
     @infosall = Array.new
     @tags.each do |tag|
       @infosall = @infosall + Info.tagged_with(tag)
@@ -10,6 +11,17 @@ class UsersController < ApplicationController
 
     @infosall.uniq!
     @infosall.sort! { |a,b| b.created_at <=> a.created_at }
+
+    @eventsall = Array.new
+    @tags.each do |tag|
+      @eventsall = @eventsall + Event.tagged_with(tag)
+    end
+
+    @eventsall.uniq!
+    @eventsall.sort! { |a,b| b.date <=> a.date }
+
   end
+
+
 
 end
