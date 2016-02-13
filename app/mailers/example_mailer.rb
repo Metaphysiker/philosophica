4b04f58e49106c820654981534b86aa94a09f45e
Metaphysiker <s.raess@me.com>
2016-02-13 23:04:05 +0100

    hilariously fixed preview

---
+++ app/mailers/example_mailer.rb
@@ -0,0 +1,18 @@
+class ExampleMailer < ApplicationMailer
+  default from: "sandro.raess@philosophie.ch"
+
+  def sample_email(user)
+    @user = user
+    @tags = @user.tag_list
+    @infosall = Array.new
+    @tags.each do |tag|
+      @infosall = @infosall + Info.tagged_with(tag)
+    end
+
+    @infosall.uniq!
+    @infosall.sort! { |a,b| b.created_at <=> a.created_at }
+    mail(to: @user.email, subject: 'Sample Email')
+
+  end
+end
+
