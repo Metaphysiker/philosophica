598ca0565e587e7a64662f388cd3cd2140171c1e
Metaphysiker <s.raess@me.com>
2016-02-14 17:57:00 +0100

    next try

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -14,13 +14,6 @@ class ExampleMailer < ApplicationMailer
 
     mail(to: @user.email, subject: 'Sample Email')
 
-    mg_client = Mailgun::Client.new ENV['api_key']
-    message_params = {:from    => ENV['gmail_username'],
-                      :to      => @user.email,
-                      :subject => 'Sample Mail using Mailgun API',
-                      :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
-    mg_client.send_message ENV['domain'], message_params
-
   end
 end
 
