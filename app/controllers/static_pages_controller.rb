716240fd4ce3ad48e6f66d19a9f70a93591fc070
Metaphysiker <s.raess@me.com>
2016-02-14 09:05:56 +0100

    fixed some minor stuff

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,8 +7,7 @@ class StaticPagesController < ApplicationController
   end
 
   def themen
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @events = Event.all
+
   end
 
   def ausbildung
@@ -16,12 +15,7 @@ class StaticPagesController < ApplicationController
   end
 
   def forschung
-    @events = Event.all
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @first_calendar_day = @date.beginning_of_month.beginning_of_week(:monday)
-    @last_calendar_day = @date.end_of_month.end_of_week(:monday)
 
-    @weeks = (@first_calendar_day..@last_calendar_day).to_a.in_groups_of(7)
   end
 
   def kalender
