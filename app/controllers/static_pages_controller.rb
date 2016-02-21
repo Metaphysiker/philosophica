41b019a130425da8823895ef21c33233892061dc
Metaphysiker <s.raess@me.com>
2016-02-21 11:58:52 +0100

    Oje

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -31,6 +31,9 @@ class StaticPagesController < ApplicationController
     @first_calendar_day = @date.beginning_of_month.beginning_of_week(:monday)
     @last_calendar_day = @date.end_of_month.end_of_week(:monday)
     @weeks = (@first_calendar_day..@last_calendar_day).to_a.in_groups_of(7)
-    @eventics = Event.where('extract(month from date) = ?', @date.month).order(:date)
+   # @eventics = Event.where('extract(month from date) = ?', @date.month).order(:date)
+    # @eventics= Event.joins(:date_events).where('extract(month from date) = ?', @date.month).order(:date)
+    @eventics = Event.joins(:date_events).where('extract(month from date_events.date) = ?', @date.month)
+    @alldays = (@first_calendar_day..@last_calendar_day)
   end
 end
