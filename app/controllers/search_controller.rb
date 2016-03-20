6cac65c8ff495fe825f95c1771de45311d88fc93
Metaphysiker <s.raess@me.com>
2016-03-20 14:21:51 +0100

    search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,18 @@
 class SearchController < ApplicationController
   def search
-    @word = params[:search_input]
-    #@pg_search_documents = PgSearch.multisearch(params[:search_input])
-    q = "%#{params[:query]}%"
 
-    eventsresult = Event.where("name LIKE ? OR content LIKE ?", "%#{params[:search_input]}%", "%#{params[:search_input]}%")
-    infosresult = Info.where("name LIKE ? OR content LIKE ?", "%#{params[:search_input]}%", "%#{params[:search_input]}%")
+    #@pg_search_documents = PgSearch.multisearch(params[:search_input])
+    eventsresult = []
+    infosresult = []
+    #eventsresult = Event.where("name LIKE ? OR content LIKE ?", "%#{params[:search_input]}%", "%#{params[:search_input]}%")
+    @word = params[:search_input].split
+    @word.each do |word|
+      eventsresult = eventsresult + Event.where("name ILIKE ? OR content ILIKE ?", "%#{word}%", "%#{word}%")
+      infosresult = Info.where("name ILIKE ? OR content ILIKE ?", "%#{word}%", "%#{word}%")
+    end
+
+    eventsresult.uniq!
+    infosresult.uniq!
 
     @events = eventsresult
     @infos = infosresult
