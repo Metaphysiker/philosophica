370904d918b3c803aea834b431eba1e0a67a57cb
Metaphysiker <s.raess@me.com>
2016-03-20 11:04:23 +0100

    added highlighter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,13 @@
 class SearchController < ApplicationController
   def search
+    @word = params[:search_input]
     #@pg_search_documents = PgSearch.multisearch(params[:search_input])
     q = "%#{params[:query]}%"
 
     eventsresult = Event.where("name LIKE ? OR content LIKE ?", "%#{params[:search_input]}%", "%#{params[:search_input]}%")
+    infosresult = Info.where("name LIKE ? OR content LIKE ?", "%#{params[:search_input]}%", "%#{params[:search_input]}%")
 
     @events = eventsresult
+    @infos = infosresult
   end
 end
